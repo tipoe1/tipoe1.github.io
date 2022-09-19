51afaa4297e20d3718c6281762dedad296bf1287
Timo P <49144141+tipoe1@users.noreply.github.com>
2022-09-19 20:16:32 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-//To-Do:
-// wenn button gedrückt input aus Textfeld übernehmen
-// dann Funktion aufrufen, die output = input ... setzt
-// und dann im Dokument anzeigen lassen
-
-
-
-
 $("#convert_btn").click(function(){
     input = $("#input").val();
     output = input.replaceAll(",", ".")
