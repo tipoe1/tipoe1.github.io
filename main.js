195c3734570707e83b625f611ab40e7dc8f379a6
Timo P <49144141+tipoe1@users.noreply.github.com>
2022-09-21 14:53:01 +0200

    Copy_btn

--- main.js
+++ main.js
@@ -3,4 +3,24 @@ $("#convert_btn").click(function(){
     output = input.replaceAll(",", ".")
     output = output.replaceAll(" ", "<br>")
     $("#output").html(output)
-})
+});
+
+function copy(){
+    var copyText = document.getElementById("output")
+    const clipboardItem = new ClipboardItem({
+        "text/plain": new Blob(
+            [copyText.innerText],
+            { type: "text/plain" }
+        ),
+        "text/html": new Blob(
+            [copyText.outerHTML],
+            { type: "text/html" }
+        ),
+    });
+    
+    navigator.clipboard.write([clipboardItem]);
+
+};
+
+$("#copy_btn").click(copy)
+    
